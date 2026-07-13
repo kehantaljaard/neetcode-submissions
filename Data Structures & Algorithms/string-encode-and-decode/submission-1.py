ffd04aa551d7fc08766bbde7ba5eb290e526ddab
Kehan Taljaard <60061601+kehantaljaard@users.noreply.github.com>
2026-07-13 22:43:48 +0200

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        out = []
+        for i in strs:
+            out.append(f"{len(i)}{i}")
+        ret = "".join(out)
+        return ret
+        
+    def decode(self, s: str) -> List[str]:
+        out = []
+        current = ""
+        n = None
+        for c in s:
+            if n is None:
+                n = int(c)
+            if n==0: 
+                n = int(c)
+                out.append(current)
+                current=""
+            if n!=0: 
+                current+= c
+                n-=1
+        return out
