0f53ce2ee53416043bc3dd69694d3ceb0f9ac38a
Kehan Taljaard <60061601+kehantaljaard@users.noreply.github.com>
2026-07-14 14:03:22 +0200

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,28 @@
+def sqno(x, y):
+    return int(x/3) + 3*int(y/3)
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [[0]*9]*9
+        cols = [[0]*9]*9
+        sqs = [[0]*9]*9
+        for i in range(9):
+            for j in range(9):
+                val = board[i][j]
+                if val != ".":
+                    # rows
+                    if rows[i][int(val)-1] == 1:
+                        return False
+                    else: rows[i][int(val)-1] = 1
+
+                    # cols
+                    if cols[j][int(val)-1] == 1:
+                        return False
+                    else: cols[j][int(val)-1] = 1
+
+                    #squares
+                    if sqs[sqno(i, j)][int(val)-1] == 1:
+                        return False
+                    else: sqs[sqno(i,j)][int(val)-1] = 1
+        return True
